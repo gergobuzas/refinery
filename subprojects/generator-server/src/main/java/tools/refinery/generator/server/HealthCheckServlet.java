229c232cf7ec0641d09b7bb9432180da6d9bd18d
gergobuzas <buzasgergo0615@gmail.com>
2024-11-16 00:37:57 +0100

    Finished with the health check servlet

--- subprojects/generator-server/src/main/java/tools/refinery/generator/server/HealthCheckServlet.java
+++ subprojects/generator-server/src/main/java/tools/refinery/generator/server/HealthCheckServlet.java
@@ -6,7 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-class HealthCheckServlet extends HttpServlet {
+public class HealthCheckServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
